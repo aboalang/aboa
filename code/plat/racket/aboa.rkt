51de3ea64108f4dd60f30640e74784a3d139a3e2
Christopher Augustus <repos@c4augustus.com>
2024-11-12 13:01:39 +0000

    rewrite parsing and execution of procedures, currently only supporting calling $io.sof with a literal value [reescrever analisar e executar de procedimentos, agora só apoiar chamar $io.sof com uma valor literal]

--- code/plat/racket/aboa.rkt
+++ code/plat/racket/aboa.rkt
@@ -7,10 +7,13 @@
 ;;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
 ;;;
 
-(define traçar #t)
+(define traçar #f)
 
-(define (traçe form . info)
-  (if traçar (apply printf (string-append "## TRAÇO: " form "~n") info) '()))
+(define (traçe form . info) (if traçar
+  (apply  printf (string-append "## aboa traço: " form "~n") info) '()))
+
+(define (erro form . info)
+  (apply eprintf (string-append "!! aboa error: " form "~n") info))
 
 ;; READER
 
@@ -39,14 +42,17 @@
                                (eq? (caar acc) 'sc))) `((sc ,c))]
           [#\] #:when     (eq? (car acc) 'al) '(ae)]
           [#\. #:when     (eq? (car acc) 'po) '(rn)]
-          [#\. '(po)] [#\, '(ac)] [#\/ '(ba)]
-          [#\_ '(pa)] [#\[ '(al)] [#\] '(ar)] [#\# '(cm)]
-          [#\~ '(ca)] [#\( '(ei)] [#\= '(ig)] [#\) '(ef)]
-          [#\! '(fl)] [#\^ '(fu)] [#\? '(se)] [#\& '(it)]
-          [#\> '(pr)] [#\< '(re)] [#\$ '(pd)] [#\: '(ti)]
+          [#\. '(po)] [#\, '(ac)] [#\/ '(ba)] [#\_ '(pi)]
+          [#\[ '(al)] [#\] '(ar)] [#\# '(cm)] [#\~ '(ca)]
+          [#\( '(ni)] [#\= '(ig)] [#\) '(nf)] [#\! '(fl)]
+          [#\^ '(fu)] [#\* '(pr)] [#\? '(se)] [#\& '(it)]
+          [#\> '(ex)] [#\< '(re)] [#\$ '(pd)] [#\: '(ti)]
+          ;;[#\- '(tr)] ;; TODO: ### trata como alpha por agora
+          [#\-                             `((ab ,c))]
+          [_   #:when (and (char-whitespace? c) (eq? (car acc) 'eb)) '()]
+          [_   #:when (char-whitespace? c) '(eb)]
           [_   #:when (char-alphabetic? c) `((ab ,c))]
           [_   #:when (char-numeric?    c) `((nu ,c))]
-          [_   #:when (char-whitespace? c) '()]
           [_   `((ch ,c))])
         acc))
     '() ; initial acc
@@ -90,9 +96,9 @@
 (provide aboa)
 (define (aboa tokens)
   (traçe "tokens:~n~s" tokens)
-  (aval-recur tokens '() (list (current-command-line-arguments) 'argu 'inic 'nãop)))
+  (aval-recur '() tokens '() (list (current-command-line-arguments) 'argu 'inic 'nãop)))
 
-(define (aval-recur tokens env pilha)
+(define (aval-recur tz tokens env pilha)
   (if (null? tokens)
     pilha
     (let*
@@ -100,21 +106,29 @@
        [td  (if (pair? tokens) (cdr  tokens) '())]
        [tad (if (pair? td)     (cadr tokens) '())]
        [p0 (match ta
-          ['ei #:when (eq? tad 'fu) (cons 'nefu pilha)]
-          ['ei #:when (eq? tad 'pr) (cons 'nepr pilha)]
-          ['ei                      (cons 'nesi pilha)]
-          ['ef                      (cons 'nesf pilha)]
-          ['ti                      (cons 'tipo pilha)]
+          [(or 'cm 'eb 'nl)                      pilha ] ;; ignore
+          [(or 'fu 'pr) #:when (or (eq? tz 'ex)
+                                   (eq? tz 'ni)) pilha ] ;; já usado
+          ['ex #:when (eq? tad 'fu)  (cons 'exfu pilha)]
+          ['ex #:when (eq? tad 'pr)  (cons 'expr pilha)]
+          ['ni #:when (eq? tad 'fu)  (cons 'defu pilha)]
+          ['ni #:when (eq? tad 'pr)  (cons 'depr pilha)]
+          ['ni                       (cons 'nesi pilha)]
+          ['nf                       (cons 'nesf pilha)]
+          ['ti                       (cons 'tipo pilha)]
           [(list 'st s) (pilha-lite-assign pilha s)]
           [(list 'no s) (pilha-nome-assign pilha s)]
-          [_ (cond [(member ta '(pa))
+          [_ (cond [(member ta '(pi))
                     (cons   ta (cons 'opmo pilha))]
-                   [(member ta '(ac ca fl ig it pr re se sl))
+                   [(member ta '(ac ca fl ig it re se sl))
                     (cons   ta (cons 'opdi pilha))]
                    [else pilha])]
         )]
-       [p1 (cond [(realizar-opmo p0)]
-                 [(realizar-opdi p0)]
+       [p1 (cond [(eq? p0 pilha) p0]
+                 ;;[(realizar-exfu p0)]
+                 [(realizar-expr p0)]
+                 ;;[(realizar-opmo p0)]
+                 ;;[(realizar-opdi p0)]
                  [else p0])]
       )
       (if (and traçar (not (eq? p0 p1))) (begin
@@ -129,7 +143,7 @@
       ;      (realizar (λ (env) env)
       ;                env traçar "_~v_ ~v --> ~v" profund arg r)
       ;      '())
-      (aval-recur td env p1))))
+      (aval-recur ta td env p1))))
 
 (define (pilha-lite-estab pilha)
   (if (eq? 'lite (cadr pilha)) pilha (cons "" (cons 'lite pilha))))
@@ -147,12 +161,50 @@
   (let ([p (pilha-nome-estab pilha)])
     (cons (string-append (car p) str) (cdr p))))
 
+(define (realizar-expr pilha)
+  (if (and (< 3  (length pilha)) (eq? 'expr (caddr pilha)))
+      (let ([ppri  (car  pilha)]
+            [pseg  (cadr pilha)]
+            [pres (cdddr pilha)])
+        ;(traçe "ppri ~v" ppri)
+        ;(traçe "pseg ~v" pseg)
+        ;(traçe "pres ~v" pres)
+        (if (not (eq? 'nome pseg))
+            (begin (erro ">* missing name of procedure")
+                   (cons ppri (cons pseg pres)))
+            (match ppri
+              ["$io.sof" (exe-nome-proc-lite ppri printf pres)]
+              ;; TODO: ### MUITO MAIS
+              [_ (begin (erro ">* unknown procedure named ~a" ppri)
+                        pres)])))
+      #f))
+
+(define (exe-nome-proc-lite nome proc pilha)
+  ;(traçe "nome ~v" nome)
+  ;(traçe "proc ~v" proc)
+  ;(traçe "pilha ~v" pilha)
+  (if (and (< 1 (length pilha)) (eq? 'lite (cadr pilha)))
+      (begin (proc (car pilha))
+             (cddr pilha))
+      (begin (erro "literal arg required for ~a" nome)
+             pilha)))
+
+#| TODO: ### AGORA NÃO USAR
+
 (define (realizar-opmo pilha)
   (if (and (not (empty? pilha)) (eq? 'opmo (cadr pilha)))
       (begin
         ;; TODO ### NO-OP FOR NOW
         (cddr pilha))
       #f))
+
+(define (realizar-pr paira paird)
+    (traçe "pr   a ~v" paira) (traçe "pr   d ~v" paird)
+    (match paird
+      ['("$io.sof" . nome) (printf (car paira))]
+      ;; TODO: ### MUITO MAIS
+      [_ (traçe "pr não funçiona: ~v > ~v" paird paira)]
+    ))
 
 (define (realizar-opdi pilha)
   (let*-values ([(opdid opdia) (splitf-at-right pilha (λ (x) (not (eq? x 'opdi))))])
@@ -172,7 +224,7 @@
         #f)))
 
 (define (realizar-pr paira paird)
-    (traçe "pr paira ~v" paira) (traçe "pr paird ~v" paird)
+    (traçe "pr   a ~v" paira) (traçe "pr   d ~v" paird)
     (match paird
       ['("$io.sof" . nome) (printf (car paira))]
       ;; TODO: ### MUITO MAIS
@@ -181,3 +233,4 @@
 
 (define (realizar proc env traçar form . info)
   (traçe form info) (proc env))
+|#
